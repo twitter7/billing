cb8aebc8402fbd1e94fa6244e125704fcd36d267
EC2 Default User <ec2-user@ip-172-31-34-139.us-east-2.compute.internal>
2022-11-25 11:04:06 +0000

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#this script is for deployment of new customer care script for epcor
+#the script covers use of location of customers to direct promotions and sales
+##please do not hard code
+#collect use information about customers as needed
+
